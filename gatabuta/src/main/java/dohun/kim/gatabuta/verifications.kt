b48bce527a9dace1ca44af717d23570aabf3bf6d
kimdohun <kimdohun0104@gmail.com>
2020-10-11 17:56:39 +0900

    [0.0.1] verify interactions

--- gatabuta/src/main/java/dohun/kim/gatabuta/verifications.kt
+++ gatabuta/src/main/java/dohun/kim/gatabuta/verifications.kt
@@ -1,7 +1,23 @@
 package dohun.kim.gatabuta
 
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
+
+sealed class Interactions {
+
+    object NoInteraction : Interactions()
+    object NoMoreInteraction : Interactions()
+}
+
+val noInteraction = Interactions.NoInteraction
+val noMoreInteraction = Interactions.NoMoreInteraction
 
 infix fun <T> T.verifyFor(method: T.() -> Unit) {
     verify(this).apply(method)
+}
+
+infix fun <T> T.verifyFor(interactions: Interactions) {
+    when (interactions) {
+        is Interactions.NoInteraction -> verifyNoInteractions(this)
+        is Interactions.NoMoreInteraction -> verifyNoMoreInteractions(this)
+    }
 }
